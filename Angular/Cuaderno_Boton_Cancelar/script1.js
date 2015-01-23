6483613d57bb3927d24b97990fb9ea547141ba34
Marcos Trejo R <mtrejo@nearbpo.com>
2015-01-23 12:24:02 -0600

    Agrego ejemplo de spring de cuaderno

--- Angular/Cuaderno_Boton_Cancelar/script1.js
+++ Angular/Cuaderno_Boton_Cancelar/script1.js
@@ -2,7 +2,7 @@ function AlumnosController($scope){
 	$scope.alumnos=[
 		{nombre:"Juan Blanco", telefono:"1234567890", curso:"Segundo ESO"},
 		{nombre:"Rosa Luxemburgo", telefono:"0987654321", curso:"Primero ESO"},
-		{nombre:"Alberto Herrera", telefono:"1122334455", curso:"Segundo ESO"},
+		{nombre:"Alberto Herrera", telefono:"1122334455", curso:"Segund o ESO"},
 		{nombre:"Ana Marino", telefono:"6677889900", curso:"Tercero ESO"}
 	];
   $scope.Save=function(){
